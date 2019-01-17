584b260987cb3e0bd8a834e34cf07c2d1c25ba9b
Hezser <contact.sergiohernandez@gmail.com>
2019-01-17 02:45:41 +0000

    1. SOLVED OUT_OF_MEMORY ERROR! 2. Added a protected mode to the prover that implements a timeout for formulas that take too long.

--- test/MolleTests.java
+++ test/MolleTests.java
@@ -58,7 +58,7 @@ public class MolleTests {
     public void crossValidateRandomInputFile() throws InvalidNumberOfPropositionsException, IncompatibleFrameConditionsException {
         InputGenerator inputGenerator = new InputGenerator();
         ModalSystem system = new ModalSystem("K");
-        ArrayList<String> formulas = inputGenerator.generateFormulas(10000, 50, 2);
+        ArrayList<String> formulas = inputGenerator.generateFormulas(1000, 50, 2);
         crossValidate(formulas, system);
     }
 
@@ -85,12 +85,17 @@ public class MolleTests {
     void crossValidate(ArrayList<String> formulas, ModalSystem system) throws IncompatibleFrameConditionsException {
 
         InputGenerator inputGenerator = new InputGenerator();
-        Prover prover = new Prover(false);
+        Prover prover = new Prover(false, false);
         MolleAdapter molleAdapter = new MolleAdapter();
+
+        System.out.println();
+        System.out.println("=================================================");
+        System.out.println();
 
         for (int i=0; i<formulas.size(); i++) {
             try {
                 Assertions.assertTrue(prover.proveFormula(formulas.get(i), system) == molleAdapter.proveFormula(inputGenerator.translateFormulaToMolle(formulas.get(i))), ("The validity of the formula " + formulas.get(i) + " is not consistent. " + i + " formulas have been successfully tested previously."));
+                System.out.println("Cross-validated " + (i+1) + " formulas successfully");
             } catch (UnrecognizableFormulaException e) {
                 e.printMessage();
             } catch (MolleUnrecognizedFormulaException e) {
